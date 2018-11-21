d1bc271ac201b8f5f0b47b7c3cd69d96f563174c
Dan Schrimpsher <dan.schrimpsher@gmail.com>
2018-11-21 15:22:49 -0600

    Changes to Krigin

--- bomb_simulation/kriging.py
+++ bomb_simulation/kriging.py
@@ -6,7 +6,7 @@ from bomb_simulation.grid import Grid
 class Kriging:
     def __init__(self, heat_map):
         self.nugget = 0
-        self.range = 25
+        self.range = 8 #1/3 of range
         self.sill = 12
         self.sv_matrix = None
         self.lag_matrix = None
@@ -37,34 +37,55 @@ class Kriging:
             column = 0
 
     def calculate_sv_matrix(self):
-        sv = lambda t: self.sill*(1 - exp(-3*t**2/self.range**2)) if t < self.range else 0
+        # sv = lambda t: self.nugget + self.sill*(1 - exp(-t/self.range)) if t < self.range and t != 0 else 0
+        sv = lambda t: self.nugget + self.sill*(1 - exp(-t/self.range)) if t != 0 else 0
         self.sv_matrix = np.array([[sv(h) for h in row] for row in self.lag_matrix])
-        self.sv_matrix = np.c_[self.sv_matrix, np.ones(len(self.points))]
-        self.sv_matrix = np.r_[self.sv_matrix, [np.ones(len(self.points)+1)]]
-        self.sv_matrix[-1, -1] = 0
+
+        self.sv_matrix = np.c_[self.sv_matrix, np.zeros(len(self.points))]
+        self.sv_matrix = np.c_[self.sv_matrix, np.zeros(len(self.points))]
+        self.sv_matrix = np.c_[self.sv_matrix, np.zeros(len(self.points))]
+        self.sv_matrix = np.r_[self.sv_matrix, [np.zeros(len(self.points)+3)]]
+        self.sv_matrix = np.r_[self.sv_matrix, [np.zeros(len(self.points)+3)]]
+        self.sv_matrix = np.r_[self.sv_matrix, [np.zeros(len(self.points)+3)]]
+
+        num_rows = len(self.points) +3
+        num_colmuns = len(self.points) + 3
+        count = 0
+        for point in self.points:
+            self.sv_matrix[num_rows-1][count] = point[1]
+            self.sv_matrix[num_rows-2][count] = point[0]
+            self.sv_matrix[num_rows-3][count] = 1
+            self.sv_matrix[count][num_colmuns-1] = point[1]
+            self.sv_matrix[count][num_colmuns-2] = point[0]
+            self.sv_matrix[count][num_colmuns-3] = 1
+            count += 1
+        #print(self.sv_matrix)
 
     def calculate_prediction_point(self, pX, pY):
         pp_lag = lambda t: sqrt(pow(t[0] - pX, 2) + pow(t[1] - pY, 2))
         self.pp = np.array([pp_lag(row) for row in self.points])
 
     def calculate_sv_pp(self):
-        ppsv = lambda t: self.sill*(1 - exp(-3*t**2/self.range**2)) if t < self.range else 0
+        # ppsv = lambda t: self.sill*(1 - exp(-t/self.range)) if t < self.range and t != 0 else 0
+        ppsv = lambda t: self.nugget + self.sill*(1 - exp(-t/self.range)) if t != 0 else 0
         self.ppsv = np.array([ppsv(h) for h in self.pp])
-        self.ppsv = np.r_[self.ppsv, np.ones(1)]
+        self.ppsv = np.r_[self.ppsv, np.ones(3)]
 
     def calculate_weights(self):
         temp = np.linalg.inv(self.sv_matrix)
         self.weights = np.dot(temp, self.ppsv)
         self.weights = np.delete(self.weights, -1, 0)
+        self.weights = np.delete(self.weights, -1, 0)
+        self.weights = np.delete(self.weights, -1, 0)
 
     def calculate_z(self):
         z = lambda t: self.heat_map.cells[t[0]][t[1]]
         self.z_matrix = np.array([z(p) for p in self.points])
-        self.pp_z = np.inner(self.weights, self.z_matrix)
-        if self.pp_z > 10:
-            self.pp_z = 11
-        elif self.pp_z < 0:
-            self.pp_z = -1
+        self.pp_z = np.inner(self.z_matrix, self.weights)
+        # sum = 0
+        # for i in range(len(self.z_matrix)):
+        #     sum += self.z_matrix[i] * self.weights[i]
+        # self.pp_z = sum
 
     def setup(self):
         self.get_points()
@@ -83,19 +104,36 @@ if __name__ == "__main__":
     np.set_printoptions(linewidth=300, precision=3)
     heat_map = Grid(16, 16)
     heat_map.init_bomb(3, 3, 10)
-    # heat_map.cells[10][0] = 5
-    # heat_map.cells[7][1] = 6
-    # heat_map.cells[13][1] = 6
-    # heat_map.cells[9][3] = 8
-    # heat_map.cells[13][2] = 7
-    # heat_map.cells[4][0] = 4
-    # heat_map.cells[8][8] = 8
-    # heat_map.cells[12][8] = 8
-    # heat_map.cells[10][9] = 7
+    # heat_map.cells[0][0] = 1
+    # heat_map.cells[1][0] = 2
+    # heat_map.cells[2][0] = 4
+    # heat_map.cells[0][1] = 5
+    # heat_map.cells[0][2] = 6
+    # heat_map.cells[2][2] = 27
+
 
     k = Kriging(heat_map)
     k.setup()
-    for x in range(heat_map.width):
-        for y in range(heat_map.height):
+    #print('\nLag')
+    #print(k.lag_matrix)
+
+    #print('\nSV')
+    #print(k.sv_matrix)
+
+
+
+    #print('\nPP')
+    #print(k.pp)
+    #print('\nPP SV')
+    #print(k.ppsv)
+
+    #print('\nWeights')
+    #print(k.weights)
+    #print('\nZ')
+    #print(k.pp_z)
+
+
+    for x in range(16):
+        for y in range(16):
             result = k.get_estimate(x, y)
-            print("Estimate for (%2d,%2d)" % (x, y), str("%.2f" % result), heat_map.cells[x][y])
+            print("Estimate for (%2d,%2d)" % (x, y), str("%4.1f" % result), heat_map.cells[x][y], ' Error ' + str("%.1f" % (result - heat_map.cells[x][y])))
